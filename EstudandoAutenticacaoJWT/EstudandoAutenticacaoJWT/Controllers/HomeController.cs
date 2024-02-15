f6d3e796092394bb35a8fee91070b0be020d328c
Robson Junior <robsonjunior1994@gmail.com>
2024-02-15 15:58:15 -0300

    First commit

---
+++ EstudandoAutenticacaoJWT/EstudandoAutenticacaoJWT/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using EstudandoAutenticacaoJWT.Models;
+using EstudandoAutenticacaoJWT.Repositories;
+using EstudandoAutenticacaoJWT.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace EstudandoAutenticacaoJWT.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+            // Recupera o usuário
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            // Verifica se o usuário existe
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            // Gera o Token
+            var token = TokenService.GenerateToken(user);
+
+            // Oculta a senha
+            user.Password = "";
+
+            // Retorna os dados
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => String.Format("Autenticado - {0},{1},{2}", User.Identity.Name, User.Identity.AuthenticationType, User.Identity.IsAuthenticated);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+
+    }
+}
---
+++ EstudandoAutenticacaoJWT/EstudandoAutenticacaoJWT/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using EstudandoAutenticacaoJWT.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EstudandoAutenticacaoJWT.Repositories
+{
+    public class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { Id = 1, Username = "batman", Password = "batman", Role = "manager" });
+            users.Add(new User { Id = 2, Username = "robin", Password = "robin", Role = "employee" });
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == password.ToLower()).FirstOrDefault();
+        }
+    }
+}
